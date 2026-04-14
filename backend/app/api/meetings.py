7017fa9a5aa22b2543d6036894599601ebf8f7c5
shivachaudhary46 <shivachaudhary4116@gmail.com>
2026-04-14 13:07:01 +0545

    remove auth for testing

--- backend/app/api/meetings.py
+++ backend/app/api/meetings.py
@@ -16,20 +16,20 @@ GET
 @router.post("/booking/")
 async def handle_booking(request: Request): 
 
-    # Verifying the request from CAL.com 
-    signature = request.headers.get("X-Cal-Signature-256")
-    body = await request.body() 
+    # # Verifying the request from CAL.com 
+    # signature = request.headers.get("X-Cal-Signature-256")
+    # body = await request.body() 
 
-    # Digest and encode the signature by hmac and hashlib.sha256 algorithm 
-    expected = hmac.new(
-        CAL_SECRET.encode(), 
-        body, 
-        hashlib.sha256
-    ).hexdigest() 
+    # # Digest and encode the signature by hmac and hashlib.sha256 algorithm 
+    # expected = hmac.new(
+    #     CAL_SECRET.encode(), 
+    #     body, 
+    #     hashlib.sha256
+    # ).hexdigest() 
 
-    # If Signature does not matches then Signature is not valid 
-    if signature != f"sha256={expected}":
-        raise HTTPException(status_code=401, detail="Invalid Signature")
+    # # If Signature does not matches then Signature is not valid 
+    # if signature != f"sha256={expected}":
+    #     raise HTTPException(status_code=401, detail="Invalid Signature")
 
     data = await request.json()
     payload = data.get("payload", {})
