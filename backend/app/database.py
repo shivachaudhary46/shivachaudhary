feb6fd0556be76e6204450f7c517bf60d8cf3b7d
shivachaudhary46 <shivachaudhary4116@gmail.com>
2026-04-16 14:23:26 +0545

    Deployment was crashed because of database connection problem

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,17 +2,17 @@ import psycopg2
 import os
 
 # Connection 
-# conn = psycopg2.connect(
-#     host=os.getenv("PGHOST"),
-#     database=os.getenv("PGDATABASE"),
-#     user=os.getenv("PGUSER"),
-#     password=os.getenv("PGPASSWORD"),
-#     port=os.getenv("PGPORT")
-# )
-
 conn = psycopg2.connect(
-    host="localhost", 
-    database="MeetingScheduler", 
-    user="postgres", 
-    password="shivachaudhary"
+    host=os.getenv("PGHOST"),
+    database=os.getenv("PGDATABASE"),
+    user=os.getenv("PGUSER"),
+    password=os.getenv("PGPASSWORD"),
+    port=os.getenv("PGPORT")
 )
+
+# conn = psycopg2.connect(
+#     host="localhost", 
+#     database="MeetingScheduler", 
+#     user="postgres", 
+#     password="shivachaudhary"
+# )
